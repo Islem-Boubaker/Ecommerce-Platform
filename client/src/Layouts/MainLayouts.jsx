c515018fba4d82e65696e9a916e33158bc1f7f06
Islem-Boubaker <islemboubaker93@gmail.com>
2025-09-08 00:42:34 +0100

    fix code of MainLayouts.jsx

--- client/src/Layouts/MainLayouts.jsx
+++ client/src/Layouts/MainLayouts.jsx
@@ -1,5 +1,5 @@
-import Header from "../Components/header";
-import Footer from "../Components/footer";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 function MainLayouts({ children }) {
     return (
