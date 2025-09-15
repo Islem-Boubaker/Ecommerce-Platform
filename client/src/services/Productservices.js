aa73d521aa5c4e066475fe58da63b4c4dea0b660
Islem-Boubaker <islemboubaker93@gmail.com>
2025-09-15 22:04:38 +0100

    add ProductService with CRUD operations for products

---
+++ client/src/services/Productservices.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+
+export const getProducts = async () => {
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/products`);
+  return response.data; // always return the data
+};
+
+
+export const getProductById = async (id) => {
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/products/${id}`);
+  return response.data;
+};
+
+
+export const createProduct = async (productData) => {
+  const response = await axios.post(`${import.meta.env.VITE_API_URL}/products`, productData);
+  return response.data;
+};
+
+
+export const deleteProduct = async (id) => {
+  const response = await axios.delete(`${import.meta.env.VITE_API_URL}/products/${id}`);
+  return response.data;
+};
